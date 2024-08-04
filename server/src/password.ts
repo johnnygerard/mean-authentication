d3de3dafe003c74dc29188c84e8a399433a6be8f
Johnny Gérard <john@jgerard.dev>
2024-08-04 22:26:18 +0200

    refactor: hashing secret

--- server/src/password.ts
+++ server/src/password.ts
@@ -2,10 +2,8 @@ import { Buffer } from "node:buffer";
 import { env } from "node:process";
 import argon2 from "argon2";
 
-const getSecret = (): Buffer => {
-  if (env.ARGON2_SECRET) return Buffer.from(env.ARGON2_SECRET);
-  throw Error("ARGON2_SECRET is not set");
-};
+if (!env.ARGON2_SECRET) throw Error("ARGON2_SECRET is not set");
+const secret = Buffer.from(env.ARGON2_SECRET);
 
 /**
  * Hash a password with Argon2
@@ -19,7 +17,7 @@ export const hashPassword = async (password: string): Promise<string> =>
   argon2.hash(password, {
     memoryCost: 12288, // 12MB
     parallelism: 1,
-    secret: getSecret(),
+    secret,
     timeCost: 3,
     type: argon2.argon2id,
   });
