1a3518847b021a9a2198696e25347e19422f3168
Johnny Gérard <john@jgerard.dev>
2024-08-12 12:07:34 +0200

    refactor: convert User type into class

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,6 +1,12 @@
-export type User = {
-  id: string; // UUIDv4
-  username: string;
-  password: string;
-  createdAt: Date;
-};
+import { randomUUID } from "node:crypto";
+
+export class User {
+  // UUIDv4 (See https://www.rfc-editor.org/rfc/rfc9562#name-uuid-version-4)
+  readonly id = randomUUID();
+  readonly createdAt = new Date();
+
+  constructor(
+    public username: string,
+    public password: string,
+  ) {}
+}
