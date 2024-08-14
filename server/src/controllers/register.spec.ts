b33295a91c6a63a32f4300cdd2748990ff5fb387
Johnny Gérard <john@jgerard.dev>
2024-08-14 17:03:16 +0200

    test: add test cases to register controller

--- server/src/controllers/register.spec.ts
+++ server/src/controllers/register.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { request } from "../test-utils.js";
-import { CREATED } from "../http-status-code.js";
+import { BAD_REQUEST, CREATED } from "../http-status-code.js";
 import { mongoClient, users } from "../mongo-client.js";
 
 describe("Register Controller", () => {
@@ -10,12 +10,48 @@ describe("Register Controller", () => {
   });
 
   it("should register a new user", async () => {
+    const username = faker.internet.userName();
     const { statusCode, payload } = await request("POST", "/register", {
-      username: faker.internet.userName(),
+      username,
       password: faker.internet.password(),
     });
 
     expect(statusCode).toBe(CREATED);
     expect(payload).toBe("");
+
+    // Retrieve new user from database
+    const user = await users.findOne({ username });
+    expect(user).not.toBeNull();
+  });
+
+  it("should not register a user with an existing username", async () => {
+    const payload = {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    await request("POST", "/register", payload);
+    const { statusCode } = await request("POST", "/register", payload);
+
+    expect(statusCode).toBe(BAD_REQUEST);
+  });
+
+  it("should not register a user with an invalid username", async () => {
+    const { statusCode } = await request("POST", "/register", {
+      username: "John\u0000Doe", // Control characters not allowed
+      password: faker.internet.password(),
+    });
+
+    expect(statusCode).toBe(BAD_REQUEST);
+  });
+
+  it("should not register a user with an invalid password", async () => {
+    const username = faker.internet.userName();
+    const { statusCode } = await request("POST", "/register", {
+      username,
+      password: username + "aA@8", // Weak passwords not allowed
+    });
+
+    expect(statusCode).toBe(BAD_REQUEST);
   });
 });
