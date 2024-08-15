6dcb3f868a2c98002a2f5b7c9c13486acf58ff6e
Johnny Gérard <john@jgerard.dev>
2024-08-15 21:16:19 +0200

    feat: add logout.ts controller

--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { register } from "../controllers/register.js";
 import { login } from "../controllers/login.js";
+import { logout } from "../controllers/logout.js";
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/logout", logout);
 router.post("/register", register);
 
 export default router;
