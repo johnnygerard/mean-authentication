779a328251625076456659e156540b8c46c967ac
Johnny Gérard <john@jgerard.dev>
2024-08-18 15:43:17 +0200

    feat: update POST /logout endpoint to DELETE /session

--- server/src/controllers/delete-session.ts
+++ server/src/controllers/delete-session.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { jwtCookieOptions } from "../auth/session.js";
 import { NO_CONTENT } from "../http-status-code.js";
 
-export const logout: RequestHandler = (req, res) => {
+export const deleteSession: RequestHandler = (req, res) => {
   res.clearCookie("session", jwtCookieOptions);
   res.status(NO_CONTENT).end();
 };
--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { createAccount } from "../controllers/create-account.js";
 import { login } from "../controllers/login.js";
-import { logout } from "../controllers/logout.js";
+import { deleteSession } from "../controllers/delete-session.js";
 import { authStatus } from "../controllers/auth-status.js";
 
 const router = express.Router();
 
 router.get("/auth-status", authStatus);
 router.post("/login", login);
-router.post("/logout", logout);
+router.delete("/session", deleteSession);
 router.post("/account", createAccount);
 
 export default router;
