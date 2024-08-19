dc827adddfd2b38b3575ed4f6598bd24ceee9133
Johnny Gérard <john@jgerard.dev>
2024-08-19 21:55:01 +0200

    refactor: ApiError class

--- server/src/types/api-error.class.ts
+++ server/src/types/api-error.class.ts
@@ -1,14 +1,14 @@
-class _ApiError {
-  constructor(public error: string) {}
-}
-
 export class ApiError {
-  static NO_SESSION_COOKIE = new _ApiError("NO_SESSION_COOKIE");
-  static TOKEN_EXPIRED = new _ApiError("TOKEN_EXPIRED");
-  static INVALID_TOKEN = new _ApiError("INVALID_TOKEN");
-  static UNEXPECTED_ERROR = new _ApiError("UNEXPECTED_ERROR");
+  private constructor(public readonly error: string) {}
+
+  static readonly UNEXPECTED_ERROR = new ApiError("UNEXPECTED_ERROR");
+
+  // Authentication errors
+  static readonly NO_SESSION_COOKIE = new ApiError("NO_SESSION_COOKIE");
+  static readonly TOKEN_EXPIRED = new ApiError("TOKEN_EXPIRED");
+  static readonly INVALID_TOKEN = new ApiError("INVALID_TOKEN");
 
   // Validation errors
-  static INVALID_USERNAME = new _ApiError("INVALID_USERNAME");
-  static INVALID_PASSWORD = new _ApiError("INVALID_PASSWORD");
+  static readonly INVALID_USERNAME = new ApiError("INVALID_USERNAME");
+  static readonly INVALID_PASSWORD = new ApiError("INVALID_PASSWORD");
 }
