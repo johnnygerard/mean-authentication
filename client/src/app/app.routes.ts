fd1eb973bba781d8ff9350986cba6ef44656e622
Johnny Gérard <john@jgerard.dev>
2024-08-21 15:27:50 +0200

    feat: update routing config

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from "@angular/router";
+import { RegisterFormComponent } from "./components/register-form/register-form.component";
 import { HomeComponent } from "./components/home/home.component";
 
 export const routes: Routes = [
@@ -6,5 +7,13 @@ export const routes: Routes = [
     path: "",
     pathMatch: "full",
     component: HomeComponent,
+  },
+  {
+    path: "register",
+    component: RegisterFormComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "",
   },
 ];
