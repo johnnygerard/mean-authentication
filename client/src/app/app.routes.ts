1eeba4aa22c62acff5077f74f5339af4bcb366a8
Johnny Gérard <john@jgerard.dev>
2024-08-23 02:16:40 +0200

    feat: use lazy-loading for register route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { RegisterFormComponent } from "./components/register-form/register-form.component";
 import { HomeComponent } from "./components/home/home.component";
 
 export const routes: Routes = [
@@ -10,7 +9,9 @@ export const routes: Routes = [
   },
   {
     path: "register",
-    component: RegisterFormComponent,
+    loadComponent: async () =>
+      (await import("./components/register-form/register-form.component"))
+        .RegisterFormComponent,
   },
   {
     path: "**",
