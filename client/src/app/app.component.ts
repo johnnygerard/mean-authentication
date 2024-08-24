a943e73feb34de2f7557a53b308fcb5e4e9b6b88
Johnny Gérard <john@jgerard.dev>
2024-08-24 21:43:47 +0200

    refactor: extract navigation component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,39 +1,21 @@
 import { Component, HostListener, inject } from "@angular/core";
-import { RouterLink, RouterOutlet } from "@angular/router";
 import { AuthService } from "./services/auth.service";
-import { HttpClient } from "@angular/common/http";
-import { environment } from "../environments/environment";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from "@angular/material/button";
-import { Title } from "@angular/platform-browser";
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
   #authService = inject(AuthService);
-  #http = inject(HttpClient);
-  title = inject(Title).getTitle();
-  isAuthenticated = this.#authService.isAuthenticated;
 
   // Delay initialization to make sure the session cookie is sent with the request
   @HostListener("document:DOMContentLoaded")
   onDOMContentLoaded(): void {
     this.#authService.initAuthStatus();
-  }
-
-  deleteSession(): void {
-    this.#http.delete(`${environment.apiUrl}/session`).subscribe({
-      next: () => {
-        this.#authService.isAuthenticated.set(false);
-      },
-      error: (e) => {
-        throw e;
-      },
-    });
   }
 }
