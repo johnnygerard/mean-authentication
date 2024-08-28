e1f399f634c9ac6279275bb4d4b8efc05f7bfe6d
Johnny Gérard <john@jgerard.dev>
2024-08-29 01:46:12 +0200

    refactor: update request function parameters

--- server/src/controllers/create-account.spec.ts
+++ server/src/controllers/create-account.spec.ts
@@ -3,56 +3,60 @@ import { request } from "../test-utils.js";
 import { BAD_REQUEST, CONFLICT, CREATED } from "../http-status-code.js";
 import { users } from "../mongo-client.js";
 
-const METHOD = "POST";
-const PATH = "/account";
-
 describe("createAccount controller", () => {
+  const REQUEST_LINE = "POST /account";
+
   afterAll(async () => {
     await users.deleteMany();
   });
 
   it("should register a new user", async () => {
-    const username = faker.internet.userName();
-    const { statusCode, payload } = await request(METHOD, PATH, {
-      username,
-      password: faker.internet.password(),
-    });
-
-    expect(statusCode).toBe(CREATED);
-    expect(payload).toBe("");
-
-    // Retrieve new user from database
-    const user = await users.findOne({ username });
-    expect(user).not.toBeNull();
-  });
-
-  it("should not register a user with an existing username", async () => {
-    const credentials = {
+    const payload = {
       username: faker.internet.userName(),
       password: faker.internet.password(),
     };
 
-    await request(METHOD, PATH, credentials);
-    const { statusCode } = await request(METHOD, PATH, credentials);
+    const response = await request(REQUEST_LINE, { payload });
+
+    expect(response.statusCode).toBe(CREATED);
+    expect(response.payload).toBe("");
+
+    // Retrieve new user from database
+    const user = await users.findOne({ username: payload.username });
+    expect(user).not.toBeNull();
+  });
+
+  it("should not register a user with an existing username", async () => {
+    const payload = {
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    };
+
+    await request(REQUEST_LINE, { payload });
+    const { statusCode } = await request(REQUEST_LINE, { payload });
 
     expect(statusCode).toBe(CONFLICT);
   });
 
   it("should not register a user with an invalid username", async () => {
-    const { statusCode } = await request(METHOD, PATH, {
+    const payload = {
       username: "John\u0000Doe", // Control characters not allowed
       password: faker.internet.password(),
-    });
+    };
+
+    const { statusCode } = await request(REQUEST_LINE, { payload });
 
     expect(statusCode).toBe(BAD_REQUEST);
   });
 
   it("should not register a user with an invalid password", async () => {
     const username = faker.internet.userName();
-    const { statusCode } = await request(METHOD, PATH, {
+    const payload = {
       username,
       password: username + "aA@8", // Weak passwords not allowed
-    });
+    };
+
+    const { statusCode } = await request(REQUEST_LINE, { payload });
 
     expect(statusCode).toBe(BAD_REQUEST);
   });
--- server/src/controllers/create-session.spec.ts
+++ server/src/controllers/create-session.spec.ts
@@ -3,57 +3,52 @@ import { request } from "../test-utils.js";
 import { CREATED, FORBIDDEN } from "../http-status-code.js";
 import { users } from "../mongo-client.js";
 
-const METHOD = "POST";
-const PATH = "/session";
-
 describe("createSession controller", () => {
+  const REQUEST_LINE = "POST /session";
+
   afterAll(async () => {
     await users.deleteMany();
   });
 
   it("should create a new authentication session", async () => {
-    const credentials = {
+    const payload = {
       username: faker.internet.userName(),
       password: faker.internet.password(),
     };
 
-    await request("POST", "/account", credentials);
+    await request("POST /account", { payload });
+    const response = await request(REQUEST_LINE, { payload });
 
-    const { statusCode, headers, payload } = await request(
-      METHOD,
-      PATH,
-      credentials,
-    );
+    expect(response.statusCode).toBe(CREATED);
 
-    expect(statusCode).toBe(CREATED);
-
-    expect(headers["set-cookie"]).toBeDefined();
-    const setCookieHeaders = headers["set-cookie"] as string[];
+    expect(response.headers["set-cookie"]).toBeDefined();
+    const setCookieHeaders = response.headers["set-cookie"] as string[];
     expect(setCookieHeaders).toHaveSize(1);
     expect(setCookieHeaders[0]).toMatch(/^session=/);
 
-    expect(payload).toBe("");
+    expect(response.payload).toBe("");
   });
 
   it("should not log in non-existing user", async () => {
-    const { statusCode } = await request(METHOD, PATH, {
+    const payload = {
       username: faker.internet.userName(),
       password: faker.internet.password(),
-    });
+    };
+    const { statusCode } = await request(REQUEST_LINE, { payload });
 
     expect(statusCode).toBe(FORBIDDEN);
   });
 
   it("should not log in user with incorrect password", async () => {
-    const username = faker.internet.userName();
-    const password = faker.internet.password();
-
-    await request("POST", "/account", { username, password });
-
-    const { statusCode } = await request(METHOD, PATH, {
-      username,
+    const payload = {
+      username: faker.internet.userName(),
       password: faker.internet.password(),
-    });
+    };
+
+    await request("POST /account", { payload });
+
+    payload.password = faker.internet.password();
+    const { statusCode } = await request(REQUEST_LINE, { payload });
 
     expect(statusCode).toBe(FORBIDDEN);
   });
