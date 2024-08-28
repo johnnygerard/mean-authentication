d76fbb19a27e7cba1f4bdaad3a6c7cce55e50d5f
Johnny Gérard <john@jgerard.dev>
2024-08-28 18:27:22 +0200

    feat: add authentication rate limiter

--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -2,10 +2,16 @@ import express from "express";
 import { createAccount } from "../controllers/create-account.js";
 import { createSession } from "../controllers/create-session.js";
 import { deleteSession } from "../controllers/delete-session.js";
+import { rateLimiter } from "../middleware/rate-limiter.js";
+import ms from "ms";
 
 const router = express.Router();
+const authRateLimiter = rateLimiter(5, ms("1m"));
 
-router.route("/session").post(createSession).delete(deleteSession);
-router.post("/account", createAccount);
+router
+  .route("/session")
+  .post(authRateLimiter, createSession)
+  .delete(deleteSession);
+router.post("/account", authRateLimiter, createAccount);
 
 export default router;
