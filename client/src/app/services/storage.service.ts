9643f44abc1e87e8b04b91008b18aae121c60d4f
Johnny Gérard <john@jgerard.dev>
2024-09-04 23:13:37 +0200

    feat: improve StorageService

--- client/src/app/services/storage.service.ts
+++ client/src/app/services/storage.service.ts
@@ -6,26 +6,23 @@ import { NotificationService } from "./notification.service";
 })
 export class StorageService {
   #isPlatformServer = typeof window === "undefined";
+  #isSupported = true;
   #notifier = inject(NotificationService);
 
   constructor() {
     if (this.#isPlatformServer) return;
 
     try {
-      if (!window.localStorage) {
+      if (window.localStorage === undefined) {
+        this.#isSupported = false;
         this.#notifier.send(
           "Your browser does not support local storage." +
             " Data will not be saved.",
         );
-        return;
       }
     } catch (e) {
       if (e instanceof DOMException && e.name === "SecurityError") {
-        window.console.error(e);
-        this.#notifier.send(
-          "Your browser or site settings do not allow local storage." +
-            " Data will not be saved.",
-        );
+        this.#handleSecurityError(e);
         return;
       }
 
@@ -33,13 +30,46 @@ export class StorageService {
     }
   }
 
-  getItem(key: string): string | null {
-    return window.localStorage.getItem(key);
+  #handleSecurityError(e: DOMException): void {
+    window.console.error(e);
+    this.#notifier.send(
+      "Your browser or site settings do not allow local storage." +
+        " Data will not be saved.",
+    );
   }
 
+  get #localStorage(): Storage | null {
+    if (!this.#isSupported) return null;
+
+    try {
+      return window.localStorage;
+    } catch (e) {
+      if (e instanceof DOMException && e.name === "SecurityError") {
+        this.#handleSecurityError(e);
+        return null;
+      }
+
+      throw e;
+    }
+  }
+
+  /**
+   * Get an item from local storage
+   * @param key - Item key
+   * @returns Item value if local storage is available and has the item, otherwise null
+   */
+  getItem(key: string): string | null {
+    return this.#localStorage?.getItem(key) ?? null;
+  }
+
+  /**
+   * Set an item in local storage
+   * @param key - Item key
+   * @param value - Item value
+   */
   setItem(key: string, value: string): void {
     try {
-      window.localStorage.setItem(key, value);
+      this.#localStorage?.setItem(key, value);
     } catch (e) {
       if (e instanceof DOMException && e.name === "QuotaExceededError") {
         this.#notifier.send(
@@ -52,7 +82,11 @@ export class StorageService {
     }
   }
 
+  /**
+   * Remove an item from local storage
+   * @param key
+   */
   removeItem(key: string): void {
-    window.localStorage.removeItem(key);
+    this.#localStorage?.removeItem(key);
   }
 }
