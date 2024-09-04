26c0ca9bfa56e79044d06428d7b900bb3035e592
Johnny Gérard <john@jgerard.dev>
2024-09-04 04:14:34 +0200

    chore: suppress JSUnusedGlobalSymbols inspection

--- client/server/src/express-session.module.ts
+++ client/server/src/express-session.module.ts
@@ -1,6 +1,7 @@
 import { SessionUser } from "./types/session-user.js";
 
 declare module "express-session" {
+  // noinspection JSUnusedGlobalSymbols
   interface SessionData {
     user: SessionUser;
   }
