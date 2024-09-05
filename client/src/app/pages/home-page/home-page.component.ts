de519b297668688a95ba8281fd465ece33357084
Johnny Gérard <john@jgerard.dev>
2024-09-05 07:16:11 +0200

    feat: add register CTA

--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -6,11 +6,13 @@ import {
 } from "@angular/core";
 import { SessionService } from "../../services/session.service";
 import { Title } from "@angular/platform-browser";
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: "app-home-page",
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: "./home-page.component.html",
   styleUrl: "./home-page.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,6 +20,7 @@ import { Title } from "@angular/platform-browser";
 export class HomePageComponent {
   #session = inject(SessionService);
   #title: string = inject(Title).getTitle();
+  isAuthenticated = this.#session.isAuthenticated;
 
   heading = computed(() => {
     const user = this.#session.user();
