8ac86c8f995447391f84858a91456d4bc3d0bb16
Johnny Gérard <john@jgerard.dev>
2024-09-10 00:23:57 +0200

    feat: add authGuardFactory and isAuthenticatedGuard

---
+++ client/src/app/guards/auth-guard-factory.ts
@@ -0,0 +1,27 @@
+import { CanActivateFn, Router } from "@angular/router";
+import { inject } from "@angular/core";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { filter, map } from "rxjs";
+import { SessionService } from "../services/session.service";
+
+const isPlatformServer = typeof window === "undefined";
+
+export function authGuardFactory(isAuthenticatedGuard: boolean): CanActivateFn {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  return (route, state) => {
+    if (isPlatformServer) return false;
+
+    const router = inject(Router);
+    const session = inject(SessionService);
+
+    return toObservable(session.isAuthenticated).pipe(
+      filter((value) => value !== null),
+      map((isAuthenticated) => {
+        if (isAuthenticatedGuard)
+          return isAuthenticated ? true : router.parseUrl("/sign-in");
+
+        return isAuthenticated ? router.parseUrl("/") : true;
+      }),
+    );
+  };
+}
