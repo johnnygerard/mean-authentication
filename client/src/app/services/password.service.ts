c53a566a64e26089acb704bddbdb71428462d551
Johnny Gérard <john@jgerard.dev>
2024-09-13 04:29:53 +0200

    perf: use deferred zxcvbn library

--- client/src/app/services/password.service.ts
+++ client/src/app/services/password.service.ts
@@ -1,5 +1,11 @@
 import { Injectable, signal } from "@angular/core";
-import zxcvbn from "zxcvbn";
+import type { ZXCVBNResult } from "zxcvbn";
+
+declare global {
+  interface Window {
+    zxcvbn: (password: string, userInputs?: string[]) => ZXCVBNResult;
+  }
+}
 
 /**
  * This service's main purpose is to share password strength results between
@@ -9,5 +15,48 @@ import zxcvbn from "zxcvbn";
   providedIn: "root",
 })
 export class PasswordService {
-  result = signal(zxcvbn(""));
+  #defaultResult: ZXCVBNResult = {
+    guesses: 1,
+    guesses_log10: 0,
+    sequence: [],
+    calc_time: 1,
+    crack_times_seconds: {
+      online_throttling_100_per_hour: 36,
+      online_no_throttling_10_per_second: 0.1,
+      offline_slow_hashing_1e4_per_second: 0.0001,
+      offline_fast_hashing_1e10_per_second: 1e-10,
+    },
+    crack_times_display: {
+      online_throttling_100_per_hour: "36 seconds",
+      online_no_throttling_10_per_second: "less than a second",
+      offline_slow_hashing_1e4_per_second: "less than a second",
+      offline_fast_hashing_1e10_per_second: "less than a second",
+    },
+    score: 0,
+    feedback: {
+      warning: "",
+      suggestions: [
+        "Use a few words, avoid common phrases",
+        "No need for symbols, digits, or uppercase letters",
+      ],
+    },
+  };
+
+  result = signal(this.#defaultResult);
+  #isPlatformBrowser = typeof window === "object";
+
+  constructor() {
+    if (this.#isPlatformBrowser) this.#loadLibrary();
+  }
+
+  #loadLibrary(): void {
+    const script = window.document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/zxcvbn@4.4.2/dist/zxcvbn.js";
+    script.defer = true;
+    script.onload = () => {
+      console.debug("Test empty password", window.zxcvbn(""));
+    };
+
+    window.document.body.appendChild(script);
+  }
 }
