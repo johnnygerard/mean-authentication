6b09ef0a659552bd966238ca0ad9879f02c213f0
Johnny Gérard <john@jgerard.dev>
2024-09-15 10:57:32 +0200

    feat: make logout endpoint private

--- server/src/routes/private.ts
+++ server/src/routes/private.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { readAccount } from "../controllers/read-account.js";
+import { deleteSession } from "../controllers/delete-session.js";
 
 const router = express.Router();
 
 router.get("/account", readAccount);
+router.delete("/session", deleteSession);
 
 export default router;
--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import { createAccount } from "../controllers/create-account.js";
 import { createSession } from "../controllers/create-session.js";
-import { deleteSession } from "../controllers/delete-session.js";
 import { rateLimiter } from "../middleware/rate-limiter.js";
 import ms from "ms";
 
 const router = express.Router();
 const authRateLimiter = rateLimiter(5, ms("1m"));
 
-router
-  .route("/session")
-  .post(authRateLimiter, createSession)
-  .delete(deleteSession);
 router.post("/account", authRateLimiter, createAccount);
+router.post("/session", authRateLimiter, createSession);
 
 export default router;
