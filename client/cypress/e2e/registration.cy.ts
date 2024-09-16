156691fc7a2a5a6351a6b175d4da376092dd40b4
Johnny Gérard <john@jgerard.dev>
2024-09-16 05:51:53 +0200

    test: add tests to registration.cy.ts

--- client/cypress/e2e/registration.cy.ts
+++ client/cypress/e2e/registration.cy.ts
@@ -1,9 +1,37 @@
 import { faker } from "@faker-js/faker";
-import { CREATED } from "../../../server/src/http-status-code";
+import { CONFLICT, CREATED } from "../../../server/src/http-status-code";
+
+const REDIRECT = "/";
 
 describe("Register page", () => {
   it("should register a new user", () => {
     registerNewUser(faker.internet.userName(), faker.internet.password());
+  });
+
+  it("should block navigation when logged in", () => {
+    registerNewUser(faker.internet.userName(), faker.internet.password());
+
+    cy.visit("/register");
+    cy.location("pathname").should("eq", REDIRECT);
+  });
+
+  it("should not register an existing user", () => {
+    const username = faker.internet.userName();
+    registerNewUser(username, faker.internet.password());
+    cy.getByData("logout-button").click();
+
+    cy.visit("/register");
+    cy.intercept("POST", "/api/account").as("register");
+
+    // Fill out and submit the form
+    cy.getByData("register-form").within(() => {
+      cy.getByData("username").type(username);
+      cy.getByData("password").type(faker.internet.password());
+      cy.root().submit();
+    });
+
+    cy.wait("@register").its("response.statusCode").should("eq", CONFLICT);
+    cy.location("pathname").should("eq", "/register");
   });
 });
 
@@ -19,6 +47,6 @@ export const registerNewUser = (username: string, password: string): void => {
   });
 
   cy.wait("@register").its("response.statusCode").should("eq", CREATED);
-  cy.location("pathname").should("eq", "/");
+  cy.location("pathname").should("eq", REDIRECT);
   cy.contains(username);
 };
