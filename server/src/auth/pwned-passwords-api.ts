3e7c909c3a89b63f50d6e738da80a910e993e47e
Johnny Gérard <john@jgerard.dev>
2024-09-18 00:24:01 +0200

    feat: handle errors in isPasswordPwned function

--- server/src/auth/pwned-passwords-api.ts
+++ server/src/auth/pwned-passwords-api.ts
@@ -1,5 +1,6 @@
 import { hash } from "node:crypto";
-import axios from "axios";
+import type { AxiosResponse } from "axios";
+import axios, { AxiosError } from "axios";
 import ms from "ms";
 
 /**
@@ -9,26 +10,31 @@ import ms from "ms";
  * (only a partial digest of the password is sent).
  * @param password - Plaintext password
  * @returns `true` if the password has been exposed, `false` otherwise.
- * @throws {AxiosError} If the HTTP status code is not in the 2xx range or
- * something else fails.
  * @see https://haveibeenpwned.com/API/v3#PwnedPasswords
  */
 export const isPasswordPwned = async (password: string): Promise<boolean> => {
   const digest = hash("sha1", password).toUpperCase();
   const partialDigest = digest.slice(0, 5);
   const digestSuffix = digest.slice(partialDigest.length);
+  let response: AxiosResponse;
 
-  const response = await axios({
-    method: "GET",
-    baseURL: "https://api.pwnedpasswords.com",
-    url: `/rang/${partialDigest}`,
-    responseType: "text",
-    responseEncoding: "utf8",
-    maxContentLength: 2 ** 20, // 1 MB
-    maxRedirects: 0,
-    signal: AbortSignal.timeout(ms("1 second")), // Handle connection timeout
-    timeout: ms("1 second"), // Handle response timeout
-  });
+  try {
+    response = await axios({
+      method: "GET",
+      baseURL: "https://api.pwnedpasswords.com",
+      url: `/rang/${partialDigest}`,
+      responseType: "text",
+      responseEncoding: "utf8",
+      maxContentLength: 2 ** 20, // 1 MB
+      maxRedirects: 0,
+      signal: AbortSignal.timeout(ms("1 second")), // Handle connection timeout
+      timeout: ms("1 second"), // Handle response timeout
+    });
+  } catch (e) {
+    console.error(e);
+    if (e instanceof AxiosError) return false;
+    throw e;
+  }
 
   const text = response.data as string;
   for (const line of text.split("\r\n"))
