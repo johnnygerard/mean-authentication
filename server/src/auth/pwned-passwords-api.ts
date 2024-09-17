b489cba530afee406a22490f49f68b9265783fb2
Johnny Gérard <john@jgerard.dev>
2024-09-17 13:26:11 +0200

    feat: add pwned-passwords-api.ts

---
+++ server/src/auth/pwned-passwords-api.ts
@@ -0,0 +1,35 @@
+import { hash } from "node:crypto";
+import axios from "axios";
+
+/**
+ * Check if a password has been exposed in a data breach.
+ *
+ * This function queries the Pwned Passwords API using the k-Anonymity model
+ * (only a partial digest of the password is sent).
+ * @param password - Plaintext password
+ * @returns `true` if the password has been exposed, `false` otherwise.
+ * @throws {AxiosError} If the HTTP status code is not in the 2xx range or
+ * something else fails.
+ * @see https://haveibeenpwned.com/API/v3#PwnedPasswords
+ */
+export const isPasswordPwned = async (password: string): Promise<boolean> => {
+  const digest = hash("sha1", password).toUpperCase();
+  const partialDigest = digest.slice(0, 5);
+  const digestSuffix = digest.slice(partialDigest.length);
+
+  const response = await axios({
+    method: "GET",
+    baseURL: "https://api.pwnedpasswords.com",
+    url: `/rang/${partialDigest}`,
+    responseType: "text",
+    responseEncoding: "utf8",
+    maxContentLength: 2 ** 20, // 1 MB
+    maxRedirects: 0,
+  });
+
+  const text = response.data as string;
+  for (const line of text.split("\r\n"))
+    if (line.toUpperCase().startsWith(digestSuffix)) return true;
+
+  return false;
+};
