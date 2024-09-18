02f119c2562149fc8bf45ebc80c6ba5a0f876aa5
Johnny Gérard <john@jgerard.dev>
2024-09-19 01:30:53 +0200

    test: update password tests

--- server/src/auth/password-hashing.spec.ts
+++ server/src/auth/password-hashing.spec.ts
@@ -1,30 +1,25 @@
+import { hashPassword, verifyPassword } from "./password-hashing.js";
 import { faker } from "@faker-js/faker";
-import {
-  hashPassword,
-  isPasswordValid,
-  PASSWORD_MAX_LENGTH,
-  verifyPassword,
-} from "./password-hashing.js";
-import { cpuUsage } from "node:process";
+import { PASSWORD_MAX_LENGTH } from "../validation/password.js";
 
-const getRandomPassword = (length?: number): string => {
-  const options = {
-    length: faker.number.int({ min: length ?? 8, max: length ?? 64 }),
-  };
-  return faker.internet.password(options);
+/**
+ * Generate a fake password with random length and characters.
+ */
+const getPassword = (): string => {
+  const length = faker.number.int({ max: PASSWORD_MAX_LENGTH });
+  return faker.internet.password({ length });
 };
 
-describe("password module", () => {
+describe("Password Hashing", () => {
   let password: string;
 
   beforeEach(() => {
-    password = getRandomPassword();
+    password = getPassword();
   });
 
-  describe("hashPassword", () => {
+  describe("The hashPassword function", () => {
     it("should produce a digest", async () => {
       const digest = await hashPassword(password);
-
       expect(typeof digest).toBe("string");
     });
 
@@ -38,69 +33,22 @@ describe("password module", () => {
 
     it("should produce a digest with Argon2id", async () => {
       const digest = await hashPassword(password);
-
       expect(digest).toContain("$argon2id$");
     });
   });
 
-  describe("verifyPassword", () => {
+  describe("The verifyPassword function", () => {
     it("should return true for a matching password", async () => {
       const digest = await hashPassword(password);
-      const matches = await verifyPassword(digest, password);
-
-      expect(matches).toBeTrue();
+      await expectAsync(verifyPassword(digest, password)).toBeResolvedTo(true);
     });
 
     it("should return false for a non-matching password", async () => {
       const digest = await hashPassword(password);
-      const matches = await verifyPassword(digest, getRandomPassword());
 
-      expect(matches).toBeFalse();
-    });
-  });
-
-  describe("isPasswordValid", () => {
-    it("should return true for a strong password", () => {
-      const username = faker.internet.userName();
-      const password = "F4`j84ZjMLNm[B~d";
-
-      expect(isPasswordValid(password, username)).toBeTrue();
-    });
-
-    it("should return false for a weak password", () => {
-      const username = faker.internet.userName();
-      // Reusing the username in the password is vulnerable to dictionary attacks
-      // while adding LUDS characters is not enough to make it strong
-      const password = username + "aB1@";
-
-      expect(isPasswordValid(password, username)).toBeFalse();
-    });
-
-    it("should allow long passwords (at least 64 characters)", () => {
-      const password = getRandomPassword(64);
-
-      expect(isPasswordValid(password)).toBeTrue();
-    });
-
-    it("should instantly reject very long passwords", () => {
-      const password = getRandomPassword(1000);
-      const startUsage = cpuUsage();
-
-      expect(isPasswordValid(password)).toBeFalse();
-
-      const usage = cpuUsage(startUsage);
-      const latency = (usage.user + usage.system) / 1000;
-      expect(latency).toBeLessThan(10); // Should be almost instantaneous
-    });
-
-    it("should take less than 100ms to validate a password", () => {
-      const password = getRandomPassword(PASSWORD_MAX_LENGTH);
-      const startUsage = cpuUsage();
-      isPasswordValid(password);
-
-      const usage = cpuUsage(startUsage);
-      const latency = (usage.user + usage.system) / 1000;
-      expect(latency).toBeLessThan(100);
+      await expectAsync(verifyPassword(digest, getPassword())).toBeResolvedTo(
+        false,
+      );
     });
   });
 });
