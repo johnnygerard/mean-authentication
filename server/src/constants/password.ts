e2ba4b3edad539d896fe59b796fb7716c35a0e27
Johnny Gérard <john@jgerard.dev>
2024-09-20 17:25:28 +0200

    feat: add ZXCVBN_MIN_SCORE

--- server/src/constants/password.ts
+++ server/src/constants/password.ts
@@ -5,3 +5,9 @@
  * @see https://github.com/dropbox/zxcvbn?tab=readme-ov-file#runtime-latency
  */
 export const PASSWORD_MAX_LENGTH = 100;
+
+/**
+ * `zxcvbn` minimum strength score
+ * @see https://github.com/dropbox/zxcvbn?tab=readme-ov-file#usage
+ */
+export const ZXCVBN_MIN_SCORE = 3;
