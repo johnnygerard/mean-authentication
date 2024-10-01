c8a05928ad2ef71ce65ed335b4970fd425668275
Johnny Gérard <john@jgerard.dev>
2024-10-01 20:35:28 +0200

    style: reformat navigation.component.ts

--- client/src/app/components/navigation/navigation.component.ts
+++ client/src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,11 @@
+import { HttpClient } from "@angular/common/http";
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
-import { Router, RouterLink, RouterOutlet } from "@angular/router";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { HttpClient } from "@angular/common/http";
-import { SessionService } from "../../services/session.service";
-import { NotificationService } from "../../services/notification.service";
-
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
 import { APP_NAME } from "_server/constants/app";
+import { NotificationService } from "../../services/notification.service";
+import { SessionService } from "../../services/session.service";
 
 @Component({
   selector: "app-navigation",
