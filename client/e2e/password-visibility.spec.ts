b2cf2c9498e8d9e620860ca0f0fb053603e06897
Johnny Gérard <john@jgerard.dev>
2024-10-03 12:27:55 +0200

    refactor: add test.describe to password-visibility.spec.ts

--- client/e2e/password-visibility.spec.ts
+++ client/e2e/password-visibility.spec.ts
@@ -1,41 +1,43 @@
 import { faker } from "@faker-js/faker";
 import { expect, Locator, test } from "@playwright/test";
 
-let passwordInput: Locator;
-let toggle: Locator;
+test.describe("Password visibility", () => {
+  let passwordInput: Locator;
+  let toggle: Locator;
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/register");
-  const form = page.getByTestId("register-form");
-  passwordInput = form.getByTestId("password");
-  toggle = form.getByTestId("visibility-toggle");
-  await passwordInput.fill(faker.internet.password());
-});
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/register");
+    const form = page.getByTestId("register-form");
+    passwordInput = form.getByTestId("password");
+    toggle = form.getByTestId("visibility-toggle");
+    await passwordInput.fill(faker.internet.password());
+  });
 
-test("The password is hidden by default.", async () => {
-  await expect(passwordInput).toHaveAttribute("type", "password");
-});
+  test("The password is hidden by default.", async () => {
+    await expect(passwordInput).toHaveAttribute("type", "password");
+  });
 
-test("The password's visibility can be toggled.", async () => {
-  await toggle.click();
-  await expect(passwordInput).toHaveAttribute("type", "text");
+  test("The password's visibility can be toggled.", async () => {
+    await toggle.click();
+    await expect(passwordInput).toHaveAttribute("type", "text");
 
-  await toggle.click();
-  await expect(passwordInput).toHaveAttribute("type", "password");
-});
+    await toggle.click();
+    await expect(passwordInput).toHaveAttribute("type", "password");
+  });
 
-test("The toggle button retains focus when clicked on.", async () => {
-  await toggle.click();
-  await expect(toggle).toBeFocused();
-});
+  test("The toggle button retains focus when clicked on.", async () => {
+    await toggle.click();
+    await expect(toggle).toBeFocused();
+  });
 
-test("The toggle button is keyboard-accessible.", async ({ page }) => {
-  await passwordInput.press("Tab");
-  const toggle = page.locator(":focus");
+  test("The toggle button is keyboard-accessible.", async ({ page }) => {
+    await passwordInput.press("Tab");
+    const toggle = page.locator(":focus");
 
-  await toggle.press("Enter");
-  await expect(passwordInput).toHaveAttribute("type", "text");
+    await toggle.press("Enter");
+    await expect(passwordInput).toHaveAttribute("type", "text");
 
-  await toggle.press("Space");
-  await expect(passwordInput).toHaveAttribute("type", "password");
+    await toggle.press("Space");
+    await expect(passwordInput).toHaveAttribute("type", "password");
+  });
 });
