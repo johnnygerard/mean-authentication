5df7ffca1aa280fd8e68e11eeb1d529c2343fb4c
Johnny Gérard <john@jgerard.dev>
2024-10-03 12:25:38 +0200

    refactor: rename password to passwordInput

--- client/e2e/password-visibility.spec.ts
+++ client/e2e/password-visibility.spec.ts
@@ -1,27 +1,27 @@
 import { faker } from "@faker-js/faker";
 import { expect, Locator, test } from "@playwright/test";
 
-let password: Locator;
+let passwordInput: Locator;
 let toggle: Locator;
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/register");
   const form = page.getByTestId("register-form");
-  password = form.getByTestId("password");
+  passwordInput = form.getByTestId("password");
   toggle = form.getByTestId("visibility-toggle");
-  await password.fill(faker.internet.password());
+  await passwordInput.fill(faker.internet.password());
 });
 
 test("The password is hidden by default.", async () => {
-  await expect(password).toHaveAttribute("type", "password");
+  await expect(passwordInput).toHaveAttribute("type", "password");
 });
 
 test("The password's visibility can be toggled.", async () => {
   await toggle.click();
-  await expect(password).toHaveAttribute("type", "text");
+  await expect(passwordInput).toHaveAttribute("type", "text");
 
   await toggle.click();
-  await expect(password).toHaveAttribute("type", "password");
+  await expect(passwordInput).toHaveAttribute("type", "password");
 });
 
 test("The toggle button retains focus when clicked on.", async () => {
@@ -30,12 +30,12 @@ test("The toggle button retains focus when clicked on.", async () => {
 });
 
 test("The toggle button is keyboard-accessible.", async ({ page }) => {
-  await password.press("Tab");
+  await passwordInput.press("Tab");
   const toggle = page.locator(":focus");
 
   await toggle.press("Enter");
-  await expect(password).toHaveAttribute("type", "text");
+  await expect(passwordInput).toHaveAttribute("type", "text");
 
   await toggle.press("Space");
-  await expect(password).toHaveAttribute("type", "password");
+  await expect(passwordInput).toHaveAttribute("type", "password");
 });
