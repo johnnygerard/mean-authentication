e354be6e9dcc4b6bbc6491be24b29bfee29e7d14
Johnny Gérard <john@jgerard.dev>
2024-10-04 21:40:06 +0200

    test: migrate user registration test

---
+++ client/e2e/tests/user-registration.spec.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+import { expect, test } from "@playwright/test";
+import { CONFLICT } from "_server/constants/http-status-code";
+import { globalUser } from "../global-user";
+import { registerUser } from "../register-user.function";
+
+test.describe("User Registration", () => {
+  const REGISTER = "/register";
+  const REDIRECT = "/";
+
+  test("User registration is successful", async ({ page }) => {
+    const username = faker.internet.userName();
+
+    await test.step("Form submission is successful", async () => {
+      await registerUser(page, { username });
+    });
+
+    await test.step("User is redirected", async () => {
+      await page.waitForURL(REDIRECT);
+    });
+
+    await test.step("User is logged in", async () => {
+      await expect(page.getByTestId("logout-button")).toBeVisible();
+      await expect(page.getByText(username)).toBeVisible();
+    });
+
+    await test.step("Navigation to registration page is blocked", async () => {
+      await page.goto(REGISTER);
+      await page.waitForURL(REDIRECT);
+    });
+  });
+
+  test("User cannot register with an existing username", async ({ page }) => {
+    await registerUser(page, { username: globalUser.username }, CONFLICT);
+  });
+});
