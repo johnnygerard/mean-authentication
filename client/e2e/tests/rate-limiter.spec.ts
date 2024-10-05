84a337946644753dc7226000ad0d428b37058f34
Johnny Gérard <john@jgerard.dev>
2024-10-05 18:01:46 +0200

    test: migrate rate-limiter test

---
+++ client/e2e/tests/rate-limiter.spec.ts
@@ -0,0 +1,33 @@
+import { faker } from "@faker-js/faker";
+import { test } from "@playwright/test";
+import { TOO_MANY_REQUESTS } from "_server/constants/http-status-code";
+
+// This test should be run separately. To run it:
+// - Replace `describe.skip` with `describe.only`
+// - Set DISABLE_RATE_LIMITER to any value other than "true" in the .env file
+test.describe.skip("Rate limiter", () => {
+  test("Requests above the rate limit are blocked", async ({ page }) => {
+    // Simulate a password brute force attack
+    await page.goto("/sign-in");
+
+    const form = page.getByTestId("login-form");
+    const usernameInput = form.getByTestId("username");
+    const passwordInput = form.getByTestId("password");
+
+    const tryToLogin = async (loginAttempts = 0): Promise<void> => {
+      if (loginAttempts === 20)
+        throw new Error(`Test failed after ${loginAttempts} attempts`);
+
+      const responsePromise = page.waitForResponse("/api/session");
+
+      await passwordInput.fill(faker.internet.password());
+      await passwordInput.press("Enter");
+
+      if ((await responsePromise).status() !== TOO_MANY_REQUESTS)
+        await tryToLogin(loginAttempts + 1);
+    };
+
+    await usernameInput.fill(faker.internet.userName());
+    await tryToLogin();
+  });
+});
