85b51c5f724375b067b5f923daa6ec98b47e743a
Johnny Gérard <john@jgerard.dev>
2024-10-14 00:42:41 +0200

    feat: rate limit the GET /account endpoint

--- server/src/routes/private.ts
+++ server/src/routes/private.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import ms from "ms";
 import { deleteSession } from "../controllers/delete-session.js";
 import { readAccount } from "../controllers/read-account.js";
+import { rateLimiter } from "../middleware/rate-limiter.js";
 
 const router = express.Router();
 
-router.get("/account", readAccount);
+router.get("/account", rateLimiter(10, ms("1 minute")), readAccount);
 router.delete("/session", deleteSession);
 
 export default router;
