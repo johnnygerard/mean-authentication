c7fee4d69dd0c6893be702cd3171da28d85a212d
Johnny Gérard <john@jgerard.dev>
2024-10-16 04:04:40 +0200

    refactor: remove redundant type check

--- server/src/controllers/read-account.ts
+++ server/src/controllers/read-account.ts
@@ -1,19 +1,12 @@
 import { RequestHandler } from "express";
 import { ObjectId } from "mongodb";
-import { NOT_FOUND, OK, UNAUTHORIZED } from "../constants/http-status-code.js";
+import { NOT_FOUND, OK } from "../constants/http-status-code.js";
 import { users } from "../database/mongo-client.js";
 
 export const readAccount: RequestHandler = async (req, res, next) => {
   try {
-    const sessionUser = req.session.user;
-
-    if (sessionUser === undefined) {
-      res.status(UNAUTHORIZED).end();
-      return;
-    }
-
     const user = await users.findOne(
-      { _id: new ObjectId(sessionUser._id) },
+      { _id: new ObjectId(req.session.user!._id) },
       { projection: { _id: 0, password: 0 } },
     );
 
