dc3af3dab8d8796783e8ec407c26aad5e38856da
Johnny Gérard <john@jgerard.dev>
2024-10-20 00:11:54 +0200

    feat: add SESSION_ID_ENTROPY to security.ts

---
+++ server/src/constants/security.ts
@@ -0,0 +1,6 @@
+/**
+ * Minimum recommended session ID entropy.
+ * @see https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-id-entropy
+ * @see https://owasp.org/www-community/vulnerabilities/Insufficient_Session-ID_Length
+ */
+export const SESSION_ID_ENTROPY = 64;
