769c75f32f45817816a11248891e5e267f2d1543
Johnny Gérard <john@jgerard.dev>
2024-10-21 12:58:39 +0200

    feat: add SESSION_SECRETS to env.ts

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -24,6 +24,19 @@ const getVar = (key: string, fallback: string): string => {
 export const ARGON2_SECRET = getVar("ARGON2_SECRET", "argon2-secret");
 export const SESSION_SECRET_1 = getVar("SESSION_SECRET_1", "session-secret-1");
 
+/**
+ * Comma-separated list of session secrets to secure session cookies while
+ * allowing for secret rotation.
+ *
+ * The first secret is always used to secure new session cookies.
+ * Subsequent secrets are used in order if verification fails with the first secret.
+ * Old secrets must be removed after the maximum session timeout has passed.
+ * @example
+ * SESSION_SECRETS=old_secret
+ * SESSION_SECRETS=new_secret,old_secret
+ */
+export const SESSION_SECRETS = getVar("SESSION_SECRETS", "session-secret");
+
 export const MONGODB_CONNECTION_URL = getVar(
   "MONGODB_CONNECTION_URL",
   "mongodb://localhost:27017/?directConnection=true",
