05cacbc34303759ef570a8c95d389fc57a80dc48
Johnny Gérard <john@jgerard.dev>
2024-10-21 20:21:23 +0200

    test: improve aes.spec.ts

--- server/src/crypto/aes.spec.ts
+++ server/src/crypto/aes.spec.ts
@@ -1,4 +1,4 @@
-import type { Buffer } from "node:buffer";
+import { Buffer } from "node:buffer";
 import { getRandomBuffer } from "../test/faker-extensions.js";
 import { decrypt, encrypt } from "./aes.js";
 
@@ -7,31 +7,30 @@ const KEY_LENGTH = 16; // 128-bit key
 describe("AES encryption and decryption", () => {
   let data: Buffer;
   let key: Buffer;
+  let encrypted: Buffer;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     data = getRandomBuffer();
     key = getRandomBuffer(KEY_LENGTH);
+    encrypted = await encrypt(data, key);
   });
 
-  it("should encrypt and decrypt data", async () => {
-    const encrypted = await encrypt(data, key);
-    const decrypted = decrypt(encrypted, key);
-
+  it("should successfully encrypt data", async () => {
     expect(data.equals(encrypted)).toBeFalse();
+  });
+
+  it("should successfully decrypt encrypted data", async () => {
+    const decrypted = decrypt(encrypted, key);
     expect(data.equals(decrypted)).toBeTrue();
   });
 
   it("should throw an error when decrypting with an incorrect key", async () => {
-    const encrypted = await encrypt(data, key);
     const incorrectKey = getRandomBuffer(KEY_LENGTH);
-
     expect(() => decrypt(encrypted, incorrectKey)).toThrow();
   });
 
   it("should encrypt the same data differently each time", async () => {
-    const encrypted1 = await encrypt(data, key);
-    const encrypted2 = await encrypt(data, key);
-
-    expect(encrypted1.equals(encrypted2)).toBeFalse();
+    const other = await encrypt(data, key);
+    expect(encrypted.equals(other)).toBeFalse();
   });
 });
