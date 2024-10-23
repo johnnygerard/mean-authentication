7fbd1f932f88463dc6918424c97d214ddb5b5799
Johnny Gérard <john@jgerard.dev>
2024-10-23 10:50:39 +0200

    feat: replace parentKey with userId in SessionStore

--- server/src/types/session-store.class.ts
+++ server/src/types/session-store.class.ts
@@ -5,34 +5,33 @@ import { ServerSession } from "./server-session.js";
 /**
  * Session store abstraction.
  *
- * Sessions belonging to the same user are grouped by a parent key that should be
- * distinct from the user ID to limit information disclosure.
+ * Sessions belonging to the same user are grouped by user ID.
  */
 export abstract class SessionStore {
   /**
    * Create a new user session.
    * @param session - The initial session data
-   * @param parentKey - The session's parent key
+   * @param userId - The user ID
    * @returns The generated session ID
    */
-  abstract add(session: ServerSession, parentKey: string): Promise<string>;
+  abstract add(session: ServerSession, userId: string): Promise<string>;
 
   /**
    * Read a user session.
-   * @param parentKey - The session's parent key
+   * @param userId - The user ID
    * @param id - The session ID
    * @returns The session data or null if not found or expired
    */
-  abstract get(parentKey: string, id: string): Promise<ServerSession | null>;
+  abstract get(userId: string, id: string): Promise<ServerSession | null>;
 
   /**
    * Delete a user session.
    *
    * This method always returns successfully, even if the session does not exist.
-   * @param parentKey - The session's parent key
+   * @param userId - The user ID
    * @param id - The session ID
    */
-  abstract delete(parentKey: string, id: string): Promise<void>;
+  abstract delete(userId: string, id: string): Promise<void>;
 
   /**
    * Generate a new session ID.
