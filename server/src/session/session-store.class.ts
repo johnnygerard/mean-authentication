acf33666a73b7281db7ea324dd010361856626e7
Johnny Gérard <john@jgerard.dev>
2024-10-23 10:58:39 +0200

    refactor: rename some SessionStore methods and parameters

--- server/src/session/session-store.class.ts
+++ server/src/session/session-store.class.ts
@@ -14,30 +14,33 @@ export abstract class SessionStore {
    * @param userId - The user ID
    * @returns The generated session ID
    */
-  abstract add(session: ServerSession, userId: string): Promise<string>;
+  abstract create(session: ServerSession, userId: string): Promise<string>;
 
   /**
    * Read a user session.
    * @param userId - The user ID
-   * @param id - The session ID
+   * @param sessionId - The session ID
    * @returns The session data or null if not found or expired
    */
-  abstract get(userId: string, id: string): Promise<ServerSession | null>;
+  abstract read(
+    userId: string,
+    sessionId: string,
+  ): Promise<ServerSession | null>;
 
   /**
    * Delete a user session.
    *
    * This method always returns successfully, even if the session does not exist.
    * @param userId - The user ID
-   * @param id - The session ID
+   * @param sessionId - The session ID
    */
-  abstract delete(userId: string, id: string): Promise<void>;
+  abstract delete(userId: string, sessionId: string): Promise<void>;
 
   /**
    * Generate a new session ID.
    * @returns A random and likely unique session ID
    */
-  protected generateId(): string {
+  protected generateSessionId(): string {
     return randomBytes(SESSION_ID_ENTROPY / 8).toString("base64url");
   }
 }
