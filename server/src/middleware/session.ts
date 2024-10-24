b212961d3a3cb0de571949d476e8a2295f4b77f6
Johnny Gérard <john@jgerard.dev>
2024-10-24 23:55:49 +0200

    feat: add session middleware

---
+++ server/src/middleware/session.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from "express";
+import store from "../session/redis-session-store.js";
+import {
+  COOKIE_NAME as SESSION_COOKIE_NAME,
+  parseSessionCookie,
+} from "../session/session-cookie.js";
+
+export const session: RequestHandler = async (req, res, next) => {
+  const sessionCookie = req.cookies[SESSION_COOKIE_NAME];
+
+  if (typeof sessionCookie === "string") {
+    const cookie = parseSessionCookie(sessionCookie);
+    if (cookie) req.session = await store.read(...cookie);
+  }
+
+  next();
+};
