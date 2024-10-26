7f2bc8f419df897261ed48f7a0c220363c8eb5e0
Johnny Gérard <john@jgerard.dev>
2024-10-26 13:29:09 +0200

    feat: add session deletion buttons

--- client/src/app/pages/sessions-page/sessions-page.component.ts
+++ client/src/app/pages/sessions-page/sessions-page.component.ts
@@ -2,25 +2,32 @@ import { HttpClient } from "@angular/common/http";
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   inject,
   signal,
 } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { MatButton } from "@angular/material/button";
+import { Router } from "@angular/router";
 import { ServerSession } from "_server/types/server-session";
 import { NotificationService } from "../../services/notification.service";
+import { SessionService } from "../../services/session.service";
 
 @Component({
   selector: "app-sessions-page",
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: "./sessions-page.component.html",
   styleUrl: "./sessions-page.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SessionsPageComponent {
   sessionCount = signal(0);
+  #destroyRef = inject(DestroyRef);
   #http = inject(HttpClient);
   #notifier = inject(NotificationService);
+  #router = inject(Router);
+  #session = inject(SessionService);
 
   constructor() {
     this.#http
@@ -34,6 +41,43 @@ export class SessionsPageComponent {
           console.error(e);
           this.#notifier.send(
             "Failed to load session data. Please try reloading the page.",
+          );
+        },
+      });
+  }
+
+  deleteAllSessions(): void {
+    this.#http
+      .delete<void>("/api/user/all-sessions")
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe({
+        next: async () => {
+          this.#session.clear();
+          await this.#router.navigateByUrl("/");
+          this.#notifier.send("All sessions have been revoked.");
+        },
+        error: (e) => {
+          console.error(e);
+          this.#notifier.send(
+            "Failed to revoke all sessions. Please try again later.",
+          );
+        },
+      });
+  }
+
+  deleteAllOtherSessions(): void {
+    this.#http
+      .delete<void>("/api/user/all-other-sessions")
+      .pipe(takeUntilDestroyed(this.#destroyRef))
+      .subscribe({
+        next: () => {
+          this.sessionCount.set(1);
+          this.#notifier.send("All other sessions have been revoked.");
+        },
+        error: (e) => {
+          console.error(e);
+          this.#notifier.send(
+            "Failed to revoke all other sessions. Please try again later.",
           );
         },
       });
