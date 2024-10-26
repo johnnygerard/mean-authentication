3af5916d8c2044e6908185c2f10d98c974d6629f
Johnny Gérard <john@jgerard.dev>
2024-10-27 01:22:06 +0200

    test: update global setup timeout

--- client/e2e/global-setup.ts
+++ client/e2e/global-setup.ts
@@ -8,7 +8,7 @@ import { registerUser } from "./register-user.function";
 const exec = promisify(child_process.exec);
 
 test("Global setup", async ({ page }) => {
-  test.setTimeout(ms("30 seconds"));
+  test.setTimeout(ms("20 seconds"));
   await exec("docker compose up --detach");
 
   console.log("Waiting for servers to be ready...");
