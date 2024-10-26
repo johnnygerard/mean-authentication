98bfe41d71f22c40a3b27875c9101b94cea4e334
Johnny Gérard <john@jgerard.dev>
2024-10-26 23:35:34 +0200

    build: improve e2e setup and teardown

--- client/e2e/global-setup.ts
+++ client/e2e/global-setup.ts
@@ -1,7 +1,19 @@
 import { test } from "@playwright/test";
+import ms from "ms";
+import child_process from "node:child_process";
+import { promisify } from "node:util";
 import { globalCredentials } from "./global-credentials";
 import { registerUser } from "./register-user.function";
 
-test("Register a new user", async ({ page }) => {
+const exec = promisify(child_process.exec);
+
+test("Global setup", async ({ page }) => {
+  test.setTimeout(ms("30 seconds"));
+
+  // Ensure the databases are in their initial state
+  await exec("docker compose down --volumes");
+  await exec("docker compose up --detach");
+
+  await exec("npm run wait:servers");
   await registerUser(page, globalCredentials);
 });
