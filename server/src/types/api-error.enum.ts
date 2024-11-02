1e5b1b0297d987e6076a8a84a211f6f1e5a863f4
Johnny Gérard <john@jgerard.dev>
2024-11-02 15:36:59 +0100

    feat: add ApiError.PARSE_ERROR

--- server/src/types/api-error.enum.ts
+++ server/src/types/api-error.enum.ts
@@ -1,5 +1,6 @@
 export const enum ApiError {
   BAD_CREDENTIALS = "Sorry, these credentials are incorrect. Please try again.",
+  PARSE_ERROR = "Invalid request. Please check your input and try again.",
   UNAUTHENTICATED = "Your session has expired. Please sign in again.",
   WRONG_PASSWORD = "Sorry, this password is incorrect. Please try again.",
 }
