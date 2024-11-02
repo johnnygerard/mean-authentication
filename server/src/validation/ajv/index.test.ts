6cabe92c2868f0e9a161e8f021042ffc02348fb7
Johnny Gérard <john@jgerard.dev>
2024-11-02 14:24:05 +0100

    test: add ajv/index.test.ts

---
+++ server/src/validation/ajv/index.test.ts
@@ -0,0 +1,50 @@
+import { faker } from "@faker-js/faker";
+import { JTDParser } from "ajv/dist/types/index.js";
+import { JTDSchemaType } from "ajv/dist/types/jtd-schema.js";
+import assert from "node:assert/strict";
+import { before, suite, test } from "node:test";
+import { ajv } from "./index.js";
+
+type Person = {
+  fullName: string;
+  isFamous: boolean;
+};
+
+const schema: JTDSchemaType<Person> = {
+  properties: {
+    fullName: { type: "string" },
+    isFamous: { type: "boolean" },
+  },
+};
+
+suite.only("ajv", () => {
+  let parser: JTDParser<Person>;
+
+  before(() => {
+    parser = ajv.compileParser(schema);
+  });
+
+  test("parses valid data", () => {
+    const person: Person = {
+      fullName: faker.person.fullName(),
+      isFamous: faker.datatype.boolean(),
+    };
+
+    const result = parser(JSON.stringify(person));
+
+    assert.deepEqual(result, person);
+    assert(!parser.message);
+  });
+
+  test("parses invalid data", () => {
+    const invalidPerson = {
+      fullName: faker.person.fullName(),
+      isFamous: faker.datatype.boolean() ? "yes" : "no",
+    };
+
+    const result = parser(JSON.stringify(invalidPerson));
+
+    assert(!result);
+    assert(parser.message);
+  });
+});
