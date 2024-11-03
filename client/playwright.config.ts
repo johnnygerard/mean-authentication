2906a8360b143b4028470f7b859c7bce38ca0653
Johnny Gérard <john@jgerard.dev>
2024-11-03 12:23:36 +0100

    test: update workers in playwright.config.ts

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -20,8 +20,12 @@ export default defineConfig({
   forbidOnly: !!process.env["CI"],
   /* Retry on CI only */
   retries: process.env["CI"] ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env["CI"] ? "100%" : "80%",
+  /*
+   * Number of virtual cores used:
+   * - CI: all available cores
+   * - Local: all but one core (to keep the system responsive)
+   */
+  workers: process.env["CI"] ? "100%" : "99%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
