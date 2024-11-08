469107ee7b3037e0e8fcde22ab696466ca24cac1
Johnny Gérard <john@jgerard.dev>
2024-11-08 21:37:18 +0100

    refactor: rename fetchDictionary to getDictionary

--- client/src/app/workers/password-strength.worker.ts
+++ client/src/app/workers/password-strength.worker.ts
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-const fetchDictionary = async (): Promise<string[]> => {
+const getDictionary = async (): Promise<string[]> => {
   const response = await fetch("app-dictionary.txt");
   const text = await response.text();
   const dictionary = text.split("\n");
@@ -25,7 +25,7 @@ const fetchDictionary = async (): Promise<string[]> => {
  * experimental.
  * @see https://angular.dev/guide/experimental/zoneless
  */
-fetchDictionary().then((dictionary) => {
+getDictionary().then((dictionary) => {
   self.onmessage = (event: MessageEvent<ZxcvbnInput>): void => {
     const { password, userInputs } = event.data;
     const result = self.zxcvbn(password, userInputs.concat(dictionary));
