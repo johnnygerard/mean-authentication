4336e3976c7987ee46fe8b7021ac586da62e70bb
Johnny Gérard <john@jgerard.dev>
2024-11-10 16:36:35 +0100

    feat: improve password validation feedback display

--- client/src/app/validators/password-validator-factory.ts
+++ client/src/app/validators/password-validator-factory.ts
@@ -1,13 +1,7 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 import { ZXCVBN_MIN_SCORE } from "_server/constants/password";
-import { ZxcvbnResult } from "_server/types/zxcvbn-result";
 import { map, Observable, of } from "rxjs";
 import { PasswordStrengthService } from "../services/password-strength.service";
-
-const getValidationErrors = (result: ZxcvbnResult): ValidationErrors | null =>
-  result.score >= ZXCVBN_MIN_SCORE
-    ? null
-    : { strength: result.feedback.warning || "Vulnerable password" };
 
 export const passwordValidatorFactory = (
   passwordStrength: PasswordStrengthService,
@@ -24,6 +18,10 @@ export const passwordValidatorFactory = (
 
     return passwordStrength
       .validate(password, userInputs)
-      .pipe(map((result) => getValidationErrors(result)));
+      .pipe(
+        map((result) =>
+          result.score >= ZXCVBN_MIN_SCORE ? null : { strength: true },
+        ),
+      );
   };
 };
