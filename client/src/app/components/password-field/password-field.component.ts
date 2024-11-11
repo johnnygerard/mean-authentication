9e92b49eef8cf92ae39ed5474dce0b6bba564c90
Johnny Gérard <john@jgerard.dev>
2024-11-11 01:47:02 +0100

    fix: discriminate strength validation reset on password label

--- client/src/app/components/password-field/password-field.component.ts
+++ client/src/app/components/password-field/password-field.component.ts
@@ -60,11 +60,13 @@ export class PasswordFieldComponent implements OnInit {
    * @see https://angular.dev/guide/forms/form-validation#creating-asynchronous-validators
    */
   ngOnInit(): void {
-    this.passwordControl()
-      .valueChanges.pipe(takeUntilDestroyed(this.#destroyRef))
-      .subscribe((value) => {
-        if (value === "") this.#strength.result$.next(zxcvbnDefaultResult);
-      });
+    if (this.label() === "Password" || this.label() === "New password") {
+      this.passwordControl()
+        .valueChanges.pipe(takeUntilDestroyed(this.#destroyRef))
+        .subscribe((value) => {
+          if (value === "") this.#strength.result$.next(zxcvbnDefaultResult);
+        });
+    }
   }
 
   togglePasswordVisibility(event: MouseEvent): void {
