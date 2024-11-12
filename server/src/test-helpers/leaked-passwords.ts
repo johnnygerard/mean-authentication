42b860a3843dc771b2f028c1600354b62763899c
Johnny Gérard <john@jgerard.dev>
2024-11-12 02:05:16 +0100

    test: add NordVPN_strong.txt and getStrongLeakedPassword function

--- server/src/test-helpers/leaked-passwords.ts
+++ server/src/test-helpers/leaked-passwords.ts
@@ -9,6 +9,23 @@ const leakedPasswords = (
 ).split("\n");
 
 /**
+ * Strong leaked passwords.
+ *
+ * `NordVPN_strong.txt` was created from the following function:
+ * ```ts
+ * leakedPasswords.filter(
+ *   (password) => zxcvbn(password).score >= ZXCVBN_MIN_SCORE,
+ * );
+ * ```
+ */
+const strongLeakedPasswords = (
+  await readFile(
+    new URL("../../data/test/NordVPN_strong.txt", import.meta.url),
+    "utf-8",
+  )
+).split("\n");
+
+/**
  * Get a random leaked password.
  *
  * Source: NordVPN data breach.
@@ -16,4 +33,14 @@ const leakedPasswords = (
  */
 export const getLeakedPassword = (): string => {
   return faker.helpers.arrayElement(leakedPasswords);
+};
+
+/**
+ * Get a random strong leaked password.
+ *
+ * Source: NordVPN data breach.
+ * @see https://github.com/danielmiessler/SecLists/blob/master/Passwords/Leaked-Databases/NordVPN.txt
+ */
+export const getStrongLeakedPassword = (): string => {
+  return faker.helpers.arrayElement(strongLeakedPasswords);
 };
